de8f513c6dda0b982cd9c9cb046022e4878d5515
SnippyRex <kentf022@gmail.com>
2023-01-22 02:30:09 +0000

    Script: Practice more JS arithmetic

--- Basic-Math.js
+++ Basic-Math.js
@@ -13,7 +13,8 @@ let finalResult = varOneTwo * varFourThree
 let evenOddResult = finalResult % 2==0
 
 if (evenOddResult == true){
-    document.write('The answer is an even number')
+    document.write('The answer is an even number' + '<br><br>')
+ 
 }
 else{
     document.write('The answer is a odd number')
@@ -21,3 +22,21 @@ else{
 
 
 //shows results of variable//
+
+// Math 2 //
+
+let result = 1.04211
+let result2 = 10
+
+result = result * result2;
+
+let finalResult2 = result.toFixed(2);
+
+document.write(typeof finalResult2);
+
+let finalNumber = Number(finalResult2);
+
+document.write('<br>' + finalNumber, typeof finalNumber)
+
+
+// Math 3 //
