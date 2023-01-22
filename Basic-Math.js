568edda25a3606d0541fc6349734a4ea066dbe00
SnippyRex <kentf022@gmail.com>
2023-01-22 02:07:45 +0000

    Script: Practice JS arithmetic & linking external JS

---
+++ Basic-Math.js
@@ -0,0 +1,23 @@
+// Math 1 //
+
+let varOne = 20;
+let varTwo = 10;
+let varThree = 5
+let varFour = 2
+
+let varOneTwo = varOne + varTwo
+let varFourThree = varFour - varThree
+
+let finalResult = varOneTwo * varFourThree
+
+let evenOddResult = finalResult % 2==0
+
+if (evenOddResult == true){
+    document.write('The answer is an even number')
+}
+else{
+    document.write('The answer is a odd number')
+    }
+
+
+//shows results of variable//
