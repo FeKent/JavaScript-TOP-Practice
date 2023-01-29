259637e31f79f7930290a923d448309b6a51cb82
SnippyRex <kentf022@gmail.com>
2023-01-29 19:57:31 +0000

    Scrtpt: Update functions to prove global scope of X

--- Playing-with-Scope/scope.js
+++ Playing-with-Scope/scope.js
@@ -6,3 +6,7 @@
 // Adding output(y/z) inside the a/b function, then allows the function to be called in browser Dev Tools. This is because they are being called from inside another function! It's in the same scope as the variables it is printing are defined in
 
 // Output() itself is available from anywere as it is defined in the global scope
+
+// Changing the output inside the a/b/ functions to (x), produces the value of x when called in the viewport. This works because x is a global variable so available inside all code
+
+//
