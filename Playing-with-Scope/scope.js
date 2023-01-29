ff4683ba03db812ce4f9d4f639b5c8e0ec60097e
SnippyRex <kentf022@gmail.com>
2023-01-29 19:46:07 +0000

    Script: Practice global and function scope in variables

---
+++ Playing-with-Scope/scope.js
@@ -0,0 +1,3 @@
+
+// Calling for output(x) defines the value in the viewport
+// Calling (y) or (z) results in an error because the values are locked inside the a and b functions, so output() can't access them when called from global scope
