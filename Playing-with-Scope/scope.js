58041134d18380ae9a80222e94c61504edfbd035
SnippyRex <kentf022@gmail.com>
2023-01-29 19:51:04 +0000

    Script: Fix scope so that y/z values can be called to viewport

--- Playing-with-Scope/scope.js
+++ Playing-with-Scope/scope.js
@@ -1,3 +1,8 @@
 
 // Calling for output(x) defines the value in the viewport
+
 // Calling (y) or (z) results in an error because the values are locked inside the a and b functions, so output() can't access them when called from global scope
+
+// Adding output(y/z) inside the a/b function, then allows the function to be called in browser Dev Tools. This is because they are being called from inside another function! It's in the same scope as the variables it is printing are defined in
+
+// Output() itself is available from anywere as it is defined in the global scope
