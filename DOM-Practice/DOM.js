3751024f915b3838e6bbb99ca8fb9be8703f7205
SnippyRex <kentf022@gmail.com>
2023-02-20 23:46:10 +0000

    Script: Use DOM to add elements with custom styling

---
+++ DOM-Practice/DOM.js
@@ -0,0 +1,28 @@
+
+const container = document.querySelector('#container');
+
+const p = document.createElement('p');
+const h3 = document.createElement('h3');
+const div = document.createElement('div');
+const h1 = document.createElement('h1');
+const p2 = document.createElement('p');
+
+div.appendChild(h1);
+div.appendChild(p2);
+
+p.textContent = 'Hey I\'m red!';
+p.style.color = 'red;';
+
+h3.textContent = 'I\'m a blue h3!';
+h3.style.color = 'blue';
+
+div.style.border = 'black';
+div.style.borderStyle = 'solid';
+div.style.backgroundColor = 'pink';
+
+h1.textContent = 'I\'m in a div';
+p2.textContent = 'ME TOO!';
+
+container.appendChild(p);
+container.appendChild(h3);
+container.appendChild(div);
